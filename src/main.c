73fba6d1fac92af1d32a7cd216936cc722a708dc
Alan Le Bouder <ale-boud@c1r5p3.42lehavre.fr>
2023-06-16 18:54:10 +0200

    small

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ale-boud <ale-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 00:07:55 by ale-boud          #+#    #+#             */
-/*   Updated: 2023/06/16 18:21:31 by ale-boud         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:26:35 by ale-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	main(void)
 		fdf_print_map_buffer(ctx, map);
 		fdf_print_buffer(ctx);
 		fdf_ctx_clear_buffer(ctx);
-		x += 0.002;
+		x += 0.005;
 		ctx->cam.x = (cos(x)) * 200 + (map->width * 10) / 2;
 		ctx->cam.y = (-sin(x)) * 200 - (map->height * 10) / 2;
 		ctx->focal.x = (-cos(x));
